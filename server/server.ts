bba59203cd2aba5e793d7d578e354176337e29fd
Rishi gupta <rishig7903@gmail.com>
2026-04-06 21:59:49 +0530

    url updated in cors

--- server/server.ts
+++ server/server.ts
@@ -21,7 +21,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:3000', "https://snap-thumb-wheat.vercel.app/"],
     credentials: true
 }))
 app.use(session({
